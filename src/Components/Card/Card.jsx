5d10d589ece2960e2dc9114996906fbda05fe832
Conor Higgins <wodowods@gmail.com>
2022-07-21 21:51:42 +0100

    format date function

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,27 +2,36 @@ import { useState } from "react";
 import "./Card.scss";
 import Button from "../Button/Button";
 
+import months from "../../data/months";
+
 const Card = ({id, name, tagline, desc, img, brewdate}) => {
+
+    let dateStr;
 
     const [showMore, setShowMore] = useState(false);
 
     const toggleShowMore = () => {
-
-        console.log("toggle");
         setShowMore(!showMore);
     }
 
+    const formatDate = (d) => {
 
-return (
+        ///Format date e.g January 1999
+            dateStr = `${months[d.substring(1,2)]} ${d.substring(3,d.length)}`;
+    } 
+
+    formatDate(brewdate);
+    
+    return (
 
     <div className="beer-card" key={id} >
         {!showMore && 
         <>
-        <img className="beer-card__img" src={img} />
-        <p className="beer-card__name">{name}</p>
-        <p className="beer-card__tagline">{tagline}</p>
-        <p className="beer-card__brew-date">First brewed: {brewdate}</p>
-        <Button className="beer-card__toggle-info" value="Read more" onClick={toggleShowMore} />
+            <img className="beer-card__img" src={img} />
+            <p className="beer-card__name">{name}</p>
+            <p className="beer-card__tagline">{tagline}</p>
+            <p className="beer-card__brew-date">First brewed: {dateStr}</p>
+            <Button className="beer-card__toggle-info" value="Read more" onClick={toggleShowMore} />
         </> }
          {showMore &&
          <>    
