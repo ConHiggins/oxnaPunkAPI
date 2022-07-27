28dd331e2b491ddf5d6e326f8c6ef1d87b02812f
Conor Higgins <wodowods@gmail.com>
2022-07-27 17:25:01 +0100

    alcohol % slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
-import Main from './Components/Main/Main';
-import NavBar from './Components/NavBar/NavBar';
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./Components/Main/Main";
+import NavBar from "./Components/NavBar/NavBar";
 
 //import beers from './data/beers';
-import { useEffect, useState, Suspense } from 'react';
-import Button from './Components/Button/Button';
+import { useEffect, useState, useRef, Suspense } from "react";
+import Button from "./Components/Button/Button";
 
-function App() { 
+function App() {
+    const [params, setParams] = useState([]);
+    const [beers, setBeers] = useState(false);
+    let minAlcPercent, maxAlcPercent;
 
-  
-  const [params, setParams] = useState([]);
-  const [beers, setBeers] = useState(false);
-  
-  
-  const getBeers = async (searchTerm) => {
-    const response = await fetch("https://api.punkapi.com/v2/beers/?" + params + "&per_page=20");
-    const data = await response.json();
-    console.log(data);
-    setBeers(data);
-  }
-  useEffect(() => { getBeers(); }, [params] );
+    const getBeers = async (params) => {
+        const response = await fetch(
+            "https://api.punkapi.com/v2/beers/?" +
+                params.join("&") +
+                "&per_page=20"
+        );
+        const data = await response.json();
+        console.log(data);
+        setBeers(data);
+    };
+    useEffect(() => {
+        getBeers(params);
+    }, [params]);
 
-//////////////////////////////////////////////////////////////////////
+    //////////////////////////// Beer name //////////////////////////////////////////
 
-  const searchTermParams = () =>{
-    ///Check for beer name param and return index
-    const currentBeerParam = params.findIndex(elem => elem.includes("beer_name"));
+    const searchTermParams = () => {
+        ///Check for beer name param and return index
+        const currentBeerParam = params.findIndex((elem) =>
+            elem.includes("beer_name")
+        );
 
-    ///Reset 
-    if(searchTerm == "") {
+        ///Reset
+        if (searchTerm == "") {
+            setParams((currState) => {
+                return [];
+            });
+        }
+        ////if URL params already contain "beer_name"
+        else if (currentBeerParam !== -1) {
+            setParams((currState) => {
+                ////If so, just update the current index
+                const tempParams = [...currState];
+                tempParams[currentBeerParam] = `beer_name=${searchTerm}`;
+                return tempParams;
+            });
+        } else {
+            setParams((currState) => {
+                ///Else push new "beer_name" to params
+                const tempParams = [...currState];
+                tempParams.push(`beer_name=${searchTerm}`);
+                return tempParams;
+            });
+        }
+    };
 
-      setParams(currState => {return [];});
+    ////////////////////////////////////////////////
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleInput = (event) => {
+        setSearchTerm(event.target.value);
+    };
+    //////////////////// Alcohol percentage ////////////////////////////////
+
+    if (beers) {
+        const alcoholPercents = beers.map((beer) => {
+            return beer.abv;
+        });
+
+        minAlcPercent = Math.min(...alcoholPercents);
+        maxAlcPercent = Math.max(...alcoholPercents);
     }
-    ////if URL params already contain "beer_name"
-    else if(currentBeerParam !== -1) {
-      
-      setParams(currState => {
-        ////If so, just update the current index
-        const tempParams = [...currState];
-        tempParams[currentBeerParam] = `beer_name=${searchTerm}`;
-        return tempParams;
-        
-      });
-    } else {
-    setParams(currState => {
-      ///Else push new "beer_name" to params 
-      const tempParams = [...currState];
-      tempParams.push(`beer_name=${searchTerm}`);
-      return tempParams; 
-    })};
-  }
-    
-  ////////////////////////////////////////////////
-  const [searchTerm, setSearchTerm] = useState("");
 
-  const handleInput = (event) => {
+    /////////////////////////////////////////////////
 
-    setSearchTerm(event.target.value);
-    
-  }
-////////////////////////////////////////////////////
-   
+    const [sliderVal, setSliderVal] = useState(maxAlcPercent);
 
-  return (
-    <>
-    
-    <NavBar className="nav-bar" 
-            searchTerm={searchTerm} 
-            handleInput={handleInput} 
-            searchTermParams={searchTermParams}
-            getBeers={getBeers} 
-            params={params} />
-    <Suspense fallback={<h1>Finding beers...</h1>}>
-      {beers && 
-      <Main className="main" arr={beers} /> }
-    </Suspense>
-    </>
-  );
+    const handleSlider = (event) => {
+        setSliderVal(event.target.value);
+    };
+
+    ////////////////////////////////////
+
+    const alcPercParams = (params) => {
+        ///Check for alc perc param and return index
+        const currentAlcPercParam = params.findIndex((elem) =>
+            elem.includes("abv")
+        );
+
+        if (currentAlcPercParam !== -1) {
+            setParams((currState) => {
+                ////If so, just update the current index
+                const tempParams = [...currState];
+                tempParams[currentAlcPercParam] = `abv_lt=${sliderVal}`;
+                return tempParams;
+            });
+        } else {
+            setParams((currState) => {
+                ///Else push new "abv" to params
+                const tempParams = [...currState];
+                tempParams.push(`abv_lt=${sliderVal}`);
+                return tempParams;
+            });
+        }
+    };
+
+    ////////////////////////////////
+
+    const updateURL = () => {
+        console.log(sliderVal);
+        searchTermParams();
+        alcPercParams(params);
+        console.log(params);
+    };
+
+    return (
+        <>
+            {beers && (
+                <>
+                    <NavBar
+                        className="nav-bar"
+                        searchTerm={searchTerm}
+                        handleInput={handleInput}
+                        updateUrl={updateURL}
+                        minAlc={minAlcPercent}
+                        maxAlc={maxAlcPercent}
+                        handleSlider={handleSlider}
+                        sliderVal={sliderVal}
+                    />
+                    <Main className="main" arr={beers} />
+                </>
+            )}
+        </>
+    );
 }
 
 export default App;
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,23 +1,54 @@
 import "./NavBar.scss";
 import Button from "../Button/Button";
+import { useRef, useState } from "react";
 
-const NavBar = ({arr, searchTerm, handleInput, getBeers, params, searchTermParams}) => {
-
+const NavBar = ({
+    searchTerm,
+    handleInput,
+    updateUrl,
+    minAlc,
+    maxAlc,
+    ref,
+    handleSlider,
+    sliderVal,
+}) => {
     const bubbles = [];
-    for(let i =0; i< 20; i++) {
-        bubbles.push(<div className={`bubble-${i}`}></div>)
+    for (let i = 0; i < 20; i++) {
+        bubbles.push(<div className={`bubble-${i}`}></div>);
     }
 
     return (
         <div className="nav-bar">
             <div className="nav-bar__searchbox-container">
-                <input className="nav-bar__searchbox" type="text" value={searchTerm} onInput={handleInput}></input>
-                <Button className="nav-bar__search-button" value="Search Beers" isSecondary={false} onClick={searchTermParams} />
+                <input
+                    className="nav-bar__searchbox"
+                    type="text"
+                    value={searchTerm}
+                    onInput={handleInput}
+                ></input>
+                <Button
+                    className="nav-bar__search-button"
+                    value="Search Beers"
+                    isSecondary={false}
+                    onClick={updateUrl}
+                />
+                <div className="nav-bar__input-container">
+                    <p className="nav-bar__input-title">Alcohol %</p>
+                    <input
+                        onChange={(event) => {
+                            handleSlider(event);
+                        }}
+                        type="range"
+                        min={minAlc}
+                        max={maxAlc}
+                        className="nav-bar__input"
+                    />
+                    <p className="nav-bar__input-title">{sliderVal}</p>
+                </div>
                 <div className="bubbles-bg">{bubbles}</div>
             </div>
-            
         </div>
-    )
-}
+    );
+};
 
 export default NavBar;
